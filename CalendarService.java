9daa676f2b39c5a8c36d7e820005535450836326
shaikhijaj313 <shaikhijaj313@gmail.com>
2026-01-15 18:25:00 +0530

    Create CalendarService.java

---
+++ CalendarService.java
@@ -0,0 +1,9 @@
+package com.example.dailybrief;
+
+import java.util.List;
+
+public class CalendarService {
+    public List<String> getEvents() {
+        return List.of("Team meeting at 10 AM", "Doctor appointment at 5 PM");
+    }
+}
